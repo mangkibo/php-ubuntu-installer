8abba8d2ac06be6cd179e1c406a4ad517d53d194
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2018-09-14 01:27:17 +0700

    fixing installer

--- install.sh
+++ install.sh
@@ -17,18 +17,16 @@ sudo apt install -y libapache2-mod-php
 sudo apt update
 
 echo "install php 5.6"
-sudo apt install -y php5.6 php5.6-mysql php5.6-intl php5.6-gd php5.6-intl php5.6-curl php5.6-mbstring php5.6-mcrypt php5.6-dom
+sudo apt install -y php5.6 php5.6-mysql php5.6-gd php5.6-intl php5.6-curl php5.6-mbstring php5.6-mcrypt php5.6-dom
 
 echo "install php 7.0"
-sudo apt install -y php7.0 php7.0-mysql php7.0-intl php7.0-gd php7.0-intl php7.0-curl php7.0-mbstring php7.0-mcrypt php7.0-dom
+sudo apt install -y php7.0 php7.0-mysql php7.0-gd php7.0-intl php7.0-curl php7.0-mbstring php7.0-mcrypt php7.0-dom
 
 echo "install php 7.1"
-sudo apt install -y php7.1 php7.1-mysql php7.1-intl php7.1-gd php7.1-intl php7.1-curl php7.1-mbstring php7.1-mcrypt php7.1-dom
+sudo apt install -y php7.1 php7.1-mysql php7.1-gd php7.1-intl php7.1-curl php7.1-mbstring php7.1-mcrypt php7.1-dom
 
 echo "install php 7.2"
-sudo apt install -y php7.2 php7.2-mysql php7.2-intl php7.2-gd php7.2-intl php7.2-curl php7.2-mbstring php7.2-mcrypt php7.2-dom
-
-
+sudo apt install -y php7.2 php7.2-mysql php7.2-gd php7.2-intl php7.2-curl php7.2-mbstring php7.2-mcrypt php7.2-dom
 
 sudo service apache2 restart
 sudo chmod +x vphp
